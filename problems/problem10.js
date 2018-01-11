c8c52a33e4b1ad6ebfd1bb66453cf5d7d1352e18
Jody Smith <jodypsmith@gmail.com>
2018-01-11 10:04:25 -0500

    9 and 10 good to go

--- problems/problem10.js
+++ problems/problem10.js
@@ -2,11 +2,19 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  
+  ["hello world"],
+  ["ALL YOUR BASE ARE BELONG TO US"],
+  ["wHaTs Up DaDiO"],
+  ["YoLoLtLt"],
+  ["Lardy dee Lardy daa"]
 ]
 
 let outputs = [
-  
+  "Hello World",
+  "All Your Base Are Belong To Us",
+  "Whats Up Dadio",
+  "Yololtlt",
+  "Lardy Dee Lardy Daa"
 ]
 
 /*
@@ -17,13 +25,20 @@ f("ALL YOUR BASE ARE BELONG"); // All Your Base Are Belong
 
 */
 function f(str) {
-    
+    //break str in words, upcase each first leter and put back into string
+    var words = str.split(" ");
+    for (var i = 0; i < words.length; i++) {
+        words[i] =words[i].charAt(0).toUpperCase() + words[i].toLowerCase().substr(1);
+        console.log(words);
+    }
+    var result = words.join(" ");
+    return result;
 }
 
 function runTest(i) {
     if(i > inputs.length) throw new Error("You do not have enough test cases");
     var expected = outputs[i];
-    var actual = f(inputs[i]);
+    var actual = f(...inputs[i]); // hate this ... thing Jaques
     assert.deepEqual(actual, expected);
 }
 
--- problems/problem9.js
+++ problems/problem9.js
@@ -2,11 +2,19 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  
+  ["There once was a man from nantucket"],
+  ["Whose nose was so long he could"],
+  ["Just Kidding"],
+  ["It wasn't his nose"],
+  ["verylongwordindeed even if its not"]
 ]
 
 let outputs = [
-  
+  "nantucket",
+  "could",
+  "Kidding",
+  "wasn't",
+  "verylongwordindeed"
 ]
 
 /*
@@ -14,13 +22,25 @@ Make this function return the longest word in the input string. If the input str
 If multiple words have the same length, return the last one that matches.
 */
 function f(str) {
-    
+    var words = str.split(" ");
+    //console.log(words);
+    var longestWord = "";
+    // right, lets compare word length, if words[i].length is > than longestWord.length then longestWord = words [i] and loop through all words
+    for (var i = 0; i < words.length; i++){
+        //console.log("test");
+        if (words[i].length >= longestWord.length) {
+            longestWord = words[i];
+            //console.log(longestWord);
+        }
+      
+    }
+    return longestWord;
 }
 
 function runTest(i) {
     if(i > inputs.length) throw new Error("You do not have enough test cases");
     var expected = outputs[i];
-    var actual = f(inputs[i]);
+    var actual = f(...inputs[i]); // well hopefully I at least remember how to split an array
     assert.deepEqual(actual, expected);
 }
 
