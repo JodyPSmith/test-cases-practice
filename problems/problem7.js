f0e5b72e980ce06c45cce9be5556745be8ad3c48
Jody Smith <jodypsmith@gmail.com>
2018-01-10 19:11:12 -0500

    completed task

--- problems/problem7.js
+++ problems/problem7.js
@@ -1,12 +1,26 @@
+//import { isString } from 'util';
+
 var assert = require('assert');
 
 // we need 7 test cases. 
 let inputs = [
-  
+    ["yolo", 1],
+    ["hello ", 3],
+    ["what", 2],
+    ["foo", -2],
+    ["boo", "G"],
+    [undefined, 4],
+    ["da", 6]
 ]
 
 let outputs = [
-  
+    "yolo",
+    "hello hello hello ",
+    "whatwhat",
+    "",
+    undefined,
+    undefined,
+    "dadadadadada"
 ]
 
 /*
@@ -20,13 +34,28 @@ f("fo", 3) // "fofofo"
 f("foo", -1) // undefined
 */
 function f(str, n) {
-    
+    //console.log(str);
+    //console.log(n);
+
+    if (str === undefined || isNaN(n) ) {
+        return undefined;
+    } else if (n <= 0) {
+        var result = "";
+        return result;
+    } else {
+        var result = "";
+        for (var i = 0; i < n; i++) {
+            //console.log(n);
+            result = result + str;
+        }
+        return result;
+    }
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
+    if (i > inputs.length) throw new Error("You do not have enough test cases");
     var expected = outputs[i];
-    var actual = f(inputs[i]);
+    var actual = f(...inputs[i]);
     assert.deepEqual(actual, expected);
 }
 
